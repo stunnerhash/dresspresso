7e69f972cd423830108f9d31ffee58561b115748
utyadav07860 <utyadav07860@outlook.com>
2024-03-07 16:36:02 +0530

    feat: shadcn-ui added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dresspresso",
@@ -16,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-800 text-slate-100 mx-auto p-4`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1>Hi</h1>
+    <Button>Button</Button>
   );
 }
