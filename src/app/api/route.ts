6326e503fb02d3a1c5a2b513814bfe4e95af8679
utyadav07860 <utyadav07860@outlook.com>
2024-03-11 01:28:48 +0530

    chore/refactor: empty files removed

--- src/app/api/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const GET = async (req:Request, res:Response) => {
-  console.log("api online");
-  return NextResponse.json({status: true, message:"Server is online"})
-}
